13c7702eb578660ef1915519c0321b3fcfc3256a
BakerAlblam <gallardo0@outlook.com>
2024-03-28 14:34:59 +0100

    GOLD

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
-      <Sidebar />
-      <MobileNav />
+      {/*  <Sidebar />
+      <MobileNav /> */}
       <div className="root-container">
         <div className="wrapper"> {children} </div>
       </div>
